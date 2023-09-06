77348d7c22854ad9c90f6f8e63957c7c8bfee2ba
Levi Mena Barreto Mesquita <120742823+LeviMena@users.noreply.github.com>
2023-09-06 20:51:18 -0300

    Create Questão 1.c

---
+++ lista 1 em c/Questão 1.c
@@ -0,0 +1,8 @@
+No primeiro caso desta questão a máquina irá executar primeiro a multiplicação e depois a divisão conforme ordem de prioridades, 
+para somente então realizar a soma, portanto o resultado será: 15;
+
+No segundo caso temos o sinal "%" que significa que usaremos o resto da divisão dos números selecionados, 
+logo mais a resposta da máquina para esta pergunta será: 3;
+
+No terceiro caso teremos operação com parênteses 
+que têm prioridades sobre as outras operações, logo resulta em: 324.
