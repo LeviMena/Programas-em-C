c640f050221819c928860c94eaad6991b4e5338a
Levi Mena Barreto Mesquita <120742823+LeviMena@users.noreply.github.com>
2023-09-06 21:59:11 -0300

    Create Questão 17.c

---
+++ lista 1 em c/Questão 17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x;
+    printf("Digite um número inteiro: ");
+    scanf("%d", &x);
+
+    printf("O módulo deste número será: %d", abs(x));
+    return 0;
+}
